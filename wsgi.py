4e48d97babeafe43781382014882410de39a7115
Stan <stan.mng@gmail.com>
2025-12-28 16:19:22 +0100

    fix(wsgi.py): add db.create_all() to load db first time

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,8 @@ from app import create_app
 # --------------- Create App
 app = create_app(os.environ.get("PRODUCTION_CONFIG"))
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     app.run()
