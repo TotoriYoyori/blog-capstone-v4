854002ebd98e7c1c4ca4b48265ab92df12e3c203
Stan <stan.mng@gmail.com>
2025-12-28 16:29:28 +0100

    fix(wsgi.py): remove db.create_all() for production

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,8 @@
 import os
-from app import create_app, db
+from app import create_app
 
 # --------------- Create App
 app = create_app(os.environ.get("PRODUCTION_CONFIG"))
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == "__main__":
     app.run()
